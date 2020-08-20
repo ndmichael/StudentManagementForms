76036dbbad195e4948697d50bdc161ca4fd2d7dd
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2020-08-20 17:34:51 +0100

    Added form footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,9 +4,12 @@ import {Box, Text, Link} from '@chakra-ui/core'
 function Footer() {
     return (
         <Box mt="5em" bg="teal" p="10px" fontSize="14px" textAlign="center">
-          <Text >Developed with <Text as="b">React </Text>/ styled with Chakra/ Form with Formik/ Validation with Yup</Text> 
+          <Text >Developed with <Text as="b">React </Text>/ 
+            styled with <Text as="b">ChakraUI </Text>/ 
+            Form Handling with <Text as="b">Formik</Text>/ 
+            Validation with <Text as="b">Yup</Text></Text> 
           <Text>Released under MiT license on GitHub</Text>
-          <Text>&copy; copyright Ukeje Micheal</Text>
+          <Text>&copy; copyright <Link href="https://github.com/ndmichael" fontSize="16px" fontWeight="900px" color="teal.500" isExternal>Ukeje Micheal</Link></Text>
         </Box>
     )
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import {Grid, Box, Text, Link} from '@chakra-ui/core'
 
 function Header() {
     return (
-        <Box bg="teal" color="white" p="5">
+        <Box color="white" p="5">
             <Box w="100%">
                 <Text fontSize="1.5em" fontWeight="700">StudentClassForm</Text>
             </Box>
